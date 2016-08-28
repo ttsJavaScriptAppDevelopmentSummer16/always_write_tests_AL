554a826b39b4447374e2d825edce4fbddbcc60d5
alexlair <alexander.lair@gmail.com>
2016-08-28 12:19:42 -0400

    Completed

---
+++ blanket/test.js
@@ -0,0 +1,17 @@
+test('getChange(215, 300) should return [50, 20, 10, 5]', function(assert) {
+  var result = getChange(215, 300); // expect an array containing [50,20,10,5]
+  var expected = [50, 20, 10, 5];
+  assert.deepEqual(result, expected);
+});
+
+test('getChange(486, 600) should equal [100, 10, 2, 2]', function(assert) {
+  var result = getChange(486, 600);
+  var expected = [100, 10, 2, 2];
+  assert.deepEqual(result, expected);
+});
+
+test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function(assert) {
+  var result = getChange(12, 400);
+  var expected = [200, 100, 50, 20, 10, 5, 2, 1];
+  assert.deepEqual(result, expected);
+});
